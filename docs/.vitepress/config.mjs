68865d50e5983e6c9c4c762b5ac2e141a7ad0b54
从哥谭市搬家到米花町 <mail4ltd@163.com>
2024-05-08 15:32:50 +0800

    feat: 更新首页

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -2,14 +2,14 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  base: "/996icu/",
-  title: "My Awesome Project",
-  description: "A VitePress Site",
+  base: '/996icu/',
+  title: 'whats996icu',
+  description: 'A VitePress Site',
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
+      { text: 'Examples', link: '/markdown-examples' },
     ],
 
     sidebar: [
@@ -17,13 +17,15 @@ export default defineConfig({
         text: 'Examples',
         items: [
           { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
-        ]
-      }
+          { text: 'Runtime API Examples', link: '/api-examples' },
+        ],
+      },
     ],
 
-    socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
-    ]
-  }
+    socialLinks: [{ icon: 'github', link: 'https://github.com/vuejs/vitepress' }],
+
+    footer: {
+      copyright: 'Copyright © 2024-present whats996icu',
+    },
+  },
 })
--- docs/index.md
+++ docs/index.md
@@ -3,23 +3,22 @@
 layout: home
 
 hero:
-  name: "My Awesome Project"
-  text: "A VitePress Site"
-  tagline: My great project tagline
+  name: "whats996icu"
+  text: "在线文档"
+  tagline: 一个中年男人搭建知识体系的执念
   actions:
     - theme: brand
-      text: Markdown Examples
-      link: /markdown-examples
+      text: 自用教程
+      link: /self-tutorial
     - theme: alt
-      text: API Examples
-      link: /api-examples
+      text: 学习路线
+      link: /roadmap
 
 features:
-  - title: Feature A
-    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
-  - title: Feature B
-    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
-  - title: Feature C
-    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: 🎉随意
+    details: 文档没有什么标准，仅仅遵循自己的思维，它是发散的
+  - title: ✔正确
+    details: 无论是教程文档，还是路线文档，都应该是开箱即用的
+  - title: ⚡高效
+    details: 尽可能的减少学习成本，让学习变得简单
 ---
-
